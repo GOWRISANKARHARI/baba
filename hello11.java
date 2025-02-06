31f0bae7bb6ffe90ba5e320881903d3f8b60e5a4
GOWRISANKARHARI <108676204+GOWRISANKARHARI@users.noreply.github.com>
2025-02-06 16:28:21 +0530

    Update hello11.java

--- hello11.java
+++ hello11.java
@@ -1,7 +1,13 @@
+import java.util.*;
 class hello
 {
 public static void main (String[] args)
-{
-System.out.println("AMOUNT WITHDRAWN EXCEEDS THE CURRENT BALANCE!");
+{	
+	Scanner sc= new Scanner (System.in);
+	System.out.println("number :");	
+	int num = sc. nextInt();
+	System.out.println("name :");
+	String name = sc.next();
+	System.out.println("name :"+name+"number:"+num);
 }
 }
