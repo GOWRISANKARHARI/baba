5c7ae090d147cb7ab7c909474b559d07a86241ee
GOWRISANKARHARI <108676204+GOWRISANKARHARI@users.noreply.github.com>
2025-02-07 10:26:52 +0530

    Update Oddeven.java

--- Oddeven.java
+++ Oddeven.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Addoddeven{
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
+  File file = new File("input1.txt");
 int size = sc.nextInt();
 int arr[] = new int[size];
 for(int i=0;i<size;i++){
