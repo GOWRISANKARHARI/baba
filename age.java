2f4bcdaad33a0490632404d1a67a7cb03442af13
GOWRISANKARHARI <108676204+GOWRISANKARHARI@users.noreply.github.com>
2025-02-07 10:58:42 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+public class age
+{ public static void main(String[] args)	
+	try{
+		File file = new File("input2.txt");
+		Scanner sc = new Scanner(file); 
+						System.out.println("Enter: "); 
+						int n= sc.nextInt();
+						System.out.println("Age: ");
+						int age=sc.nextInt();
+	}catch (FileNotFoundException e){
+        System.out.println("Error:file not found");
+        e.printStackTrace();
+    }
+}
