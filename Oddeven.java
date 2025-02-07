0d3221c63bfc9ee92ac934819c5a9b267959f530
GOWRISANKARHARI <108676204+GOWRISANKARHARI@users.noreply.github.com>
2025-02-07 10:48:35 +0530

    Update Oddeven.java

--- Oddeven.java
+++ Oddeven.java
@@ -1,33 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-public class Oddeven{
-public static void main(String[] args){
-  try {
-    Scanner sc = new Scanner(file);
-  File file = new File("input1.txt");
-    int size = sc.nextInt();
-    int arr[] = new int[size];
-      for(int i=0;i<size;i++){
-        arr[i] = sc.nextInt();
+import java.util.*;
+
+class Oddeven {
+    public static void main(String[] args) {
+        try
+            {
+                File file= new File("input1.txt");
+    	Scanner s = new Scanner(file);
+    	int size=s.nextInt();
+    	int arr[] = new int[size];
+    	
+    	for(int i=0;i<size;i++)
+    	{
+    		arr[i]=s.nextInt();
+    	}
+    	
+        
+        int max = arr[0];
+        int min = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
         }
-int odd = 0;
-int even =0;
-for(int i=0;i<size;i++){
-if(arr[i] % 2 == 0){
-even++;
-}
-else{
-odd++;
-}
-  System.out.println("EVEN NUMBER:"+even);
-  System.out.println("ODD NUMBER: "+odd);
 
-
-  }
-  catch (FileNotFoundException e){
-         System.out.println("Error in the file"); 
-    e.printStackTrace();
-  }
-}
+        System.out.println("Maximum value: " + max);
+        System.out.println("Minimum value: " + min);
+            }
+        catch (FileNotFoundException e){
+            System.out.println("Error:file not found");
+            e.printStackTrace();
+        }
+                
+    	
+    }
 }
