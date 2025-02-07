de893df04ccc97ff081769e7027dd802f6fba2b8
GOWRISANKARHARI <108676204+GOWRISANKARHARI@users.noreply.github.com>
2025-02-07 11:06:12 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+public class first
+{ public static void main(String[] args)	
+	try{
+		File file = new File("input2.txt");
+		Scanner sc = new Scanner(file); 
+						System.out.println("Name: "); 
+						String name= sc.next();
+						System.out.println("Age: ");
+						int age=sc.nextInt();
+            System.out.println("class: ");
+            String class=sc.next();
+	}catch (FileNotFoundException e){
+        System.out.println("Error:file not found");
+        e.printStackTrace();
+    }
+}
